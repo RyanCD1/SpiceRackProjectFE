fe89bad68404fd7898262007a7317b484938e60d
RyanCD1 <ryandonaghue0@gmail.com>
2021-07-28 12:51:45 +0100

    BOOT-21 #comment added Get by Id function and button to JS

---
+++ JS/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+
+const baseURL = "http://localhost:8080";
+
+const getByIdOutput = document.querySelector("#getByIdOutput");
+
+const spiceId = document.querySelector("#spiceId");
+
+const renderSpice = (spice, outputDiv) => {   
+    const spiceColumn = document.createElement('div');
+    spiceColumn.classList.add("col");
+
+    const spiceCard = document.createElement('div');
+    spiceCard.classList.add("card");
+    spiceColumn.appendChild(spiceCard);
+
+    const newSpice = document.createElement('div');
+    newSpice.classList.add("card-body");
+    
+    const spiceName = document.createElement("h3");
+    spiceName.innerText = spice.name;
+    spiceName.classList.add("card-title");
+    newSpice.appendChild(spiceName);
+
+
+    const spiceFlavourRating = document.createElement("p");
+    spiceFlavourRating.innerText = `Delicousness: ${spice.delicousness}`;
+    spiceFlavourRating.classList.add("card-text");
+    newSpice.appendChild(spiceFlavourRating);
+
+    const spiceCuisine = document.createElement("p");
+    spiceCuisine.innerText = `Cuisine: ${spice.cuisine}`; 
+    spiceCuisine.classList.add("card-text");
+    newSpice.appendChild(spiceCuisine);
+
+    const spicePrice = document.createElement("p");
+    spicePrice.innerText = `Price: ${spice.price}`; 
+    spicePrice.classList.add("card-text");
+    newSpice.appendChild(spicePrice);
+
+    spiceCard.appendChild(newSpice);
+
+    outputDiv.appendChild(spiceColumn);
+}
+
+const getSpiceById = () => {
+    axios.get(`${baseURL}/getSpice/${spiceId.value}`)
+    .then(res => {
+        const spice = res.data;
+        getByIdOutput.innerHTML = "";
+        renderSpice(spice, getByIdOutput);
+    }).catch(err => console.log(err));
+}
+
+document.querySelector("button#getByIdButton").addEventListener('click', getSpiceById);
+
+
+
